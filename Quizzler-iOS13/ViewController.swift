4f53d302116cca18ff2e39110dde38e54814426c
julielu1 <julieywlu@gmail.com>
2023-01-31 15:17:36 +1000

    Added functionality to the buttons (incomplete)

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two is equal to Six",
+        "The first man in space was bald",
+        "Chickens have combs on their head",
+        "Crickets chirp by rubbing their legs together"
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = quiz[questionNumber]
     }
-
+    @IBAction func anyButtonPressed(_ sender: UIButton) {
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 
 }
 
